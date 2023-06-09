ecd00191b91d62fd8e228281b49a76ae0cc22545
wajed <wajeed.mabroukeh@gmail.com>
2023-06-09 15:33:44 +0300

    push folder Body view as responsive

---
+++ src/Pages/Home/Components/Body Arrivals/Bodyarrivals.js
@@ -0,0 +1,13 @@
+  import "./Bodyarrivals.css"
+
+
+function Bodyarrivals() {
+    return (
+      <div className='Bodyarrivals'>
+        
+      </div>
+    );
+  }
+  
+
+  export default Bodyarrivals;
