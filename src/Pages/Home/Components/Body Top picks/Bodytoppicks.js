72cfcc301d83700eaf9ff5c0dd3b826eeb1b344e
wajed <wajeed.mabroukeh@gmail.com>
2023-06-10 01:28:16 +0300

    push folder Body Top picks  as responsive

--- src/Pages/Home/Components/Body Top picks/Bodytoppicks.js
+++ src/Pages/Home/Components/Body Top picks/Bodytoppicks.js
@@ -1,10 +1,14 @@
 import "./Bodytoppicks.css"
-
+import Toppickstitle from "./Components/Toppickstitle/Toppickstitle"
+import Bodypicks from "./Components/Bodypicks/Bodypicks"
+import Footerpicks from "./Components/Footerpicks/Footerpicks"
 
 function Bodytoppicks() {
     return (
       <div className='Bodytoppicks'>
-        
+        <Toppickstitle titlec="Top Picks For You" subtitlec="Find a bright ideal to suit your taste with our great selection of suspension, floor and table lights."/>
+        <Bodypicks />
+        <Footerpicks subtitlec="View More"/>
       </div>
     );
   }
